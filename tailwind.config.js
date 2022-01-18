3d6680e1377539599319e5855ecdf08596d5978b
Trendafil Sakarov <t_sajkarov@yahoo.com>
2022-01-18 08:41:34 +0100

    install tailwind dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,37 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    extend: {
+      minHeight: {
+        760: "760px",
+        700: "700px",
+        600: "600px",
+        400: "400px",
+      },
+      maxHeight: {
+        1000: "1000px",
+        760: "760px",
+        700: "700px",
+        600: "600px",
+        200: "200px",
+      },
+      height: {
+        xxxl: "1200px",
+        xxxxl: "1500px",
+        750: "750px",
+      },
+      width: {
+        600: "600px",
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
 };
