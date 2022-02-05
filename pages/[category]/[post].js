4ba1f8726b9e1dc684556416bcfd659393dc3025
filo <t_sajkarov@yahoo.com>
2022-02-05 02:09:42 +0100

    work on displaying HtmlTable in posts

--- pages/[category]/[post].js
+++ pages/[category]/[post].js
@@ -56,7 +56,7 @@ export default function PostPage({
         <section className="container grid grid-cols-2 auto-rows-auto w-11/12 lg:grid-cols-3 gap-4 xl:w-3/4 max-w-screen-xl mx-auto my-6">
           <article className="col-span-2 px-1 md:px-4 flex flex-col items-center">
             <PortableText
-              className="prose w-full mt-16 prose-th:text-base prose-th:py-1 prose-img:mx-auto prose-figcaption:italic prose-figcaption:text-xs prose-figcaption:text-center prose-a:text-blue-600 font-sans prose-headings:font-medium prose-headings:text-3xl prose-headings:font-serif prose-blockquote:font-serif"
+              className="prose w-full mt-16 prose-th:text-base prose-th:font-medium prose-th:py-1 prose-img:mx-auto prose-figcaption:italic prose-figcaption:text-xs prose-figcaption:text-center prose-a:text-blue-600 font-sans prose-headings:font-medium prose-headings:text-3xl prose-headings:font-serif prose-blockquote:font-serif"
               blocks={singlePost[0].body}
               serializers={serializers}
             />
