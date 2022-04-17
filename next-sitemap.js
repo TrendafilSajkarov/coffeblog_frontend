5b9d58cbb36c0ee7764d38af1c341b27b1d0f423
TrendafilSajkarov <t_sajkarov@yahoo.com>
2022-04-18 01:09:07 +0200

    fix dinamic sitemap's path from /server-sitemap.xml to /sitemap-0.xml

--- next-sitemap.js
+++ next-sitemap.js
@@ -5,9 +5,7 @@ module.exports = {
   generateRobotsTxt: true,
   robotsTxtOptions: {
     additionalSitemaps: [
-      `${
-        process.env.SITE_ADDRESS || "http://localhost:3000"
-      }/server-sitemap.xml`,
+      `${process.env.SITE_ADDRESS || "http://localhost:3000"}/sitemap-0.xml`,
       `${process.env.SITE_ADDRESS || "http://localhost:3000"}/sitemap.xml`,
     ],
   },
