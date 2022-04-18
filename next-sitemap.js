1b1e464d4a832b0522d9da6bf4fc9b6f0fc1e7cb
TrendafilSajkarov <t_sajkarov@yahoo.com>
2022-04-18 02:05:44 +0200

    remove dinamic sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,12 +3,4 @@ const siteUrl = process.env.SITE_ADDRESS || "http://localhost:3000";
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
-  exclude: ["/server-sitemap.xml"],
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      `${
-        process.env.SITE_ADDRESS || "http://localhost:3000"
-      }/server-sitemap.xml`,
-    ],
-  },
 };
