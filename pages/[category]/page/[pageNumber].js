0e879061a99c1615935430ff2f9eff04b2f31723
TrendafilSajkarov <t_sajkarov@yahoo.com>
2022-10-30 18:56:42 +0100

    added if check currentPage<0 to throw 404 on category page

--- pages/[category]/page/[pageNumber].js
+++ pages/[category]/page/[pageNumber].js
@@ -22,7 +22,11 @@ export default function CategoryPageWithNumber({
   currentPage,
 }) {
   // This includes setting the noindex header because static files always return a status 200 but the rendered not found page page should obviously not be indexed
-  if (currentPage > pages || (pages > 0 && currentPage === 0)) {
+  if (
+    currentPage > pages ||
+    (pages > 0 && currentPage === 0) ||
+    currentPage < 0
+  ) {
     return (
       <>
         <Head>
