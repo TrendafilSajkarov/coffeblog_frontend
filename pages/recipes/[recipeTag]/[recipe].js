53ef5868fda67876a50317d8b09b8db2681d8299
TrendafilSajkarov <t_sajkarov@yahoo.com>
2023-02-07 19:22:22 +0100

    added ProductGallery to recipeBody and recipeIngredients to recipe page

--- pages/recipes/[recipeTag]/[recipe].js
+++ pages/recipes/[recipeTag]/[recipe].js
@@ -305,6 +305,37 @@ export async function getStaticProps(context) {
   const recipeQuery = groq`
     *[_type == "recipe" && slug.current == "${context.params.recipe.toString()}"]{
       ...,
+      recipeIngredients[]{
+        ...,
+        markDefs[]{
+          ...,
+          _type == "internalLink" => {
+            "postSlug": @.reference->slug.current,
+            "categorySlug": @.reference->categories[0]->slug.current
+          },
+          _type == "internalRecipeLink" => {
+            "recipeSlug": @.reference->slug.current,
+            "recipeTagSlug": @.reference->recipeTags[0]->slug.current
+          },
+          _type == "internalCategoryLink" => {
+            "categorySlug": @.reference->slug.current,
+          },
+        },
+        _type == "image" => {
+          "metadata": @.asset->
+        },
+        _type == "productsGallery" => {
+          ...,
+          allProducts[]{
+            ...,
+            "asset": @.asset->
+          }
+        },
+        images[]{
+          ...,
+          "asset": @.asset->
+        }
+      },
       recipeBody[]{
         ...,
         markDefs[]{
@@ -323,6 +354,13 @@ export async function getStaticProps(context) {
         },
         _type == "image" => {
           "metadata": @.asset->
+        },
+        _type == "productsGallery" => {
+          ...,
+          allProducts[]{
+            ...,
+            "asset": @.asset->
+          }
         },
         images[]{
           ...,
